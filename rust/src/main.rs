acd3789882d9f92689c2287db8b59f8520b311c6
p31d4 <166958893+p31d4@users.noreply.github.com>
2021-03-08 19:31:00 +0000

    rust - Tetris background drawn at the screen

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,52 @@
-use tetra::graphics::{self, Color};
+use tetra::graphics::{self, Color, Rectangle};
+use tetra::graphics::mesh::{GeometryBuilder, Mesh, ShapeStyle};
 use tetra::{Context, ContextBuilder, State};
+use tetra::math::Vec2;
 
 const SCREEN_WIDTH: i32 = 620;
 const SCREEN_HEIGHT: i32 = 620;
+const GAME_SCREEN_WIDTH: f32 = 300.0;
+const GAME_SCREEN_HEIGHT: f32 = 600.0;
 
-struct GameState;
+struct GameState {
+    mesh: Mesh,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+
+        let mesh = GeometryBuilder::new()
+            .set_color(Color::BLACK)
+            .rectangle (
+                ShapeStyle::Fill,
+                Rectangle::new(10.0, 10.0,
+                    GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT),
+            )?
+            .set_color(Color::rgb(0.8, 0.6, 0.0))
+            .rectangle (
+                ShapeStyle::Stroke(10.0),
+                Rectangle::new(0.0, 0.0,
+                    SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32),
+            )?
+            .build_mesh(ctx)?;
+
+        Ok ( GameState {mesh} )
+    }
+}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::WHITE);
+
+        self.mesh.draw(ctx, Vec2::new(0.0, 0.0));
+
         Ok(())
     }
 }
 
 fn main() -> tetra::Result {
     ContextBuilder::new("Tetris", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .quit_on_escape(true)
         .build()?
-        .run(|_| Ok(GameState))
+        .run(GameState::new)
 }
